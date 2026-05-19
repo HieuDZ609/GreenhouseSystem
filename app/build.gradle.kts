923ad89215a25d2eaff2a630abd54c2d7210516b
khangdafin <khangpt.24it@vku.udn.vn>
2026-05-20 00:35:48 +0700

    sửa lại các lỗi lặt vặt ở các file xml không comment ở đầu dòng nha, sửa lại phiên bản của android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.greenhousesystem"
-        minSdk = 26
+        minSdk = 27
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
